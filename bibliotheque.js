4ceaa0a171a1bef7d8015b16da588c47e5ed9313
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-09-19 11:45:40 +0200

    add filters

--- bibliotheque.js
+++ bibliotheque.js
@@ -1,50 +1,72 @@
-const books = [
-  {
-    id: 1,
-    title: "Harry Potter",
-    author: "J.K. Rowling",
-    year: 1997,
-    genre: "Fantasy",
-    available: true,
-  },
-  {
-    id: 2,
-    title: "The Lord of the Rings",
-    author: "J.R.R. Tolkien",
-    year: 1954,
-    genre: "Fantasy",
-    available: true,
-  },
-  {
-    id: 3,
-    title: "The Da Vinci Code",
-    author: "Dan Brown",
-    year: 2003,
-    genre: "Mystery",
-    available: false,
-  },
-  {
-    id: 4,
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    year: 1937,
-    genre: "Fantasy",
-    available: true,
-  },
-  {
-    id: 5,
+var books = [
+    {
+        id: 1,
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        year: 1997,
+        genre: "Fantasy",
+        available: true,
+    },
+    {
+        id: 2,
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        year: 1954,
+        genre: "Fantasy",
+        available: true,
+    },
+    {
+        id: 3,
+        title: "The Da Vinci Code",
+        author: "Dan Brown",
+        year: 2003,
+        genre: "Mystery",
+        available: false,
+    },
+    {
+        id: 4,
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        year: 1937,
+        genre: "Fantasy",
+        available: true,
+    },
+    {
+        id: 5,
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        year: 1951,
+        genre: "Fiction",
+        available: true,
+    },
+    {
+        id: 6,
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        year: 1925,
+        genre: "Fiction",
+        available: true,
+    },
+];
+var addBook = function (book) {
+    books.push(book);
+};
+addBook({
+    id: 7,
     title: "The Catcher in the Rye",
     author: "J.D. Salinger",
     year: 1951,
     genre: "Fiction",
     available: true,
-  },
-  {
-    id: 6,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925,
-    genre: "Fiction",
-    available: true,
-  },
-];
+});
+var displayBooks = function () {
+    books.forEach(function (book) {
+        console.log(book);
+    });
+};
+displayBooks();
+var filterByTitle = function (search) {
+    return books.filter(function (book) { return book.title.includes(search); });
+};
+var results = filterByTitle("Catcher");
+console.log(results);
---
+++ bibliotheque.ts
@@ -0,0 +1,122 @@
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+  year: number;
+  genre: string;
+  available: boolean;
+};
+
+const books: Book[] = [
+  {
+    id: 1,
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    year: 1997,
+    genre: "Fantasy",
+    available: true,
+  },
+  {
+    id: 2,
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    year: 1954,
+    genre: "Fantasy",
+    available: true,
+  },
+  {
+    id: 3,
+    title: "The Da Vinci Code",
+    author: "Dan Brown",
+    year: 2003,
+    genre: "Mystery",
+    available: false,
+  },
+  {
+    id: 4,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    year: 1937,
+    genre: "Fantasy",
+    available: true,
+  },
+  {
+    id: 5,
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951,
+    genre: "Fiction",
+    available: true,
+  },
+  {
+    id: 6,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    genre: "Fiction",
+    available: true,
+  },
+];
+
+const addBook = (book: Book) => {
+  books.push(book);
+};
+
+addBook({
+  id: 7,
+  title: "The Catcher in the Rye",
+  author: "J.D. Salinger",
+  year: 1951,
+  genre: "Fiction",
+  available: true,
+});
+
+const displayBooks = () => {
+  books.forEach((book) => {
+    console.log(book);
+  });
+};
+
+displayBooks();
+
+const filterByTitle = (search: string) =>
+  books.filter(
+    (book) => book.author.includes(search) || book.title.includes(search)
+  );
+
+const results = filterByTitle("Catcher");
+
+console.log(results);
+
+const filterByGender = (genre: string) => {
+  books.filter(function (book) {
+    if (book.genre === genre) {
+      return true;
+    }
+    return false;
+  });
+};
+
+function monFiltre(book, genre) {
+  if (book.genre === genre) {
+    return true;
+  }
+  return false;
+}
+
+const filterByGender1 = (genre: string) => {
+  books.filter((book) => monFiltre(book, genre));
+};
+
+const filterByGender2 = (genre: string) => {
+  books.filter((book) => {
+    if (book.genre === genre) {
+      return true;
+    }
+    return false;
+  });
+};
+
+const filterByGender3 = (genre: string) => {
+  books.filter((book) => book.genre === genre);
+};
